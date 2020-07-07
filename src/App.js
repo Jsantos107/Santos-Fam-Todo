36f7a85b89077d8dbbe7daa468830f0239967116
github username <github email address>
2020-07-07 11:57:30 -0600

    Getting React router Dom into app also adding home button to page.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 class App extends Component {
   render(){
     return (
-      <div className="App">
-        <header className="header">
-          <img src="./Santos-TLogo.png" alt="SantosTo-Do"/>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="header">
+            <img src="./Santos-TLogo.png" alt="SantosTo-Do"/>
+            <nav className="nav-bar">
+                <button>
+                  <Link to='/' style={{textDecoration:'none', color:'white'}}>Home</Link>
+                </button>
+            </nav>
+          </header>
+          <main className='main'>
+            <Switch>
+              <Route exact path='/'>
+              </Route>
+            </Switch>
+          </main>
+        </div>
+      </Router>
     );
   }
 }
